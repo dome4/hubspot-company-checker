d557d34bb6a5e141e58da08c54a5ed7c6d0d3873
dome4 <dominic.seitz@gmx.de>
2020-09-21 09:53:43 +0200

    create main file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from prettytable import PrettyTable
 
 from utils import init
 
@@ -6,16 +7,50 @@ from utils import init
 def main():
     hubspotClient = init.initHubspotClient()
 
-    # Call the 'Get all contacts' API endpoint
-    response = hubspotClient.get(
-        'https://api.hubapi.com/contacts/v1/lists/all/contacts/all',
-        params={'count': 1}  # Return only 1 result -- for demo purposes
+    companies = [
+        's'
+    ]
+
+    for company in companies:
+        checkCompany(hubspotClient, company)
+
+
+def checkCompany(hubspotClient, searchQuery):
+
+    # create table
+    table = PrettyTable()
+    table.field_names = ["Search Query", "Company Name", "Hubspot URL"]
+
+    response = findCompanyByName(hubspotClient, searchQuery)
+
+    # response is an array
+    for match in response:
+        table.add_row([searchQuery, match.get(
+            'properties').get('name'), 'https://app.hubspot.com/contacts/6708064/company/' + match.get('id')])
+
+    print(table)
+
+
+def findCompanyByName(hubspotClient, query):
+    payload = {
+        'query': query
+    }
+
+    headers = {'content-type': 'application/json'}
+
+    # make request
+    response = hubspotClient.post(
+        'https://api.hubapi.com/crm/v3/objects/companies/search',
+        data=json.dumps(payload),
+        headers=headers
     )
 
     # Pretty-print our API result to console
-    print('Here is one Contact Record from your CRM:')
+    print('Response from your CRM:')
     print('-----------------------------------------')
-    print(json.dumps(response.json(), indent=2, sort_keys=True))
+    print('status: ' + str(response.status_code))
+
+    return response.json().get("results")
 
 
 if __name__ == '__main__':
